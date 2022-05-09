8c558569d661f49a55747cc00b5882202da34f9f
Janet Gainer-Dewar <janet.dewar@gmail.com>
2022-05-09 19:55:38 -0400

    Question mark

--- keyboards/crkbd/keymaps/jgainerdewar/keymap.c
+++ keyboards/crkbd/keymaps/jgainerdewar/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_split_3x6_3(
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO, 
         KC_CAPS, LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,     KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RALT_T(KC_QUOT), KC_NO, 
-        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,                                       KC_N, KC_M, KC_COMM, KC_DOT, KC_PSLS, KC_NO, 
+        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,                                       KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, 
         KC_BSPC, LT(LSYM,KC_SPC), LT(LNAV,KC_TAB),                                 LT(RNAV,KC_ENT), LT(RSYM,KC_SPC), KC_BSPC
     ),
     [LNAV] = LAYOUT_crkbd_leftlayer(
