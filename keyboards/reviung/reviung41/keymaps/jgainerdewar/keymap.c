29dbd858d74adf486d2947948aefa4cdcf35e2dd
Janet Gainer-Dewar <janet.dewar@gmail.com>
2022-07-21 13:13:54 -0400

    Tweak reviung map

--- keyboards/reviung/reviung41/keymaps/jgainerdewar/keymap.c
+++ keyboards/reviung/reviung41/keymaps/jgainerdewar/keymap.c
@@ -21,8 +21,7 @@ enum custom_keycodes {
     MY_PSTE,
     MY_UNDO,
     MY_FIND,
-    MY_PTSC,
-    MY_SPLT
+    MY_PTSC
 };
 
 enum layers {
@@ -76,9 +75,9 @@ LAYOUT_reviung41(\
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_reviung41(
     KC_ESC,  KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,            KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_TAB,  HR_A,     HR_S,     HR_D,     HR_F,      KC_G,            KC_H,     HR_J,     HR_K,     HR_L,     HR_QT,    KC_ENT,
+    KC_TAB,  KC_A,     HR_S,     HR_D,     HR_F,      KC_G,            KC_H,     HR_J,     HR_K,     HR_L,     HR_QT,    KC_ENT,
     KC_LSFT, KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,            KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_CAPS,
-                                MO(LSYM),   LT(LNAV,KC_ENT), LT(SYM,KC_SPC),   LT(MY_SPLT, RNAV),    MO(RSYM)
+                                MO(LSYM),   LT(LNAV,KC_ENT), LT(SYM,KC_SPC),   LT(RNAV, KC_TAB),    MO(RSYM)
   ),
 
   [LNAV] = LAYOUT_reviung41_leftlayer(
@@ -143,11 +142,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MY_PTSC:
       if (record->event.pressed) {
         SEND_STRING(SS_LGUI(SS_LSFT("4")));
-      }
-      return false;
-    case MY_SPLT:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI(" "));
       }
       return false;
   }
